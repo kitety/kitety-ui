13585975d1ff2a7048a4ed2c49df1ecc4a10f9c6
kitety <aizaizuori@gmail.com>
2020-10-28 06:51:50 +0000

    Deploying to gh-pages from master eb801d006574fba6116efb4f0dcc8cfb83bace6f

---
+++ precache-manifest.465f53f5153c337510fad50c6ff1ad55.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adde93b702e0ad2fdc27f6c8259f317",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e3ab0ec032a302c74f43.bundle.js"
+  },
+  {
+    "url": "runtime~main.e3ab0ec032a302c74f43.bundle.js"
+  },
+  {
+    "url": "vendors~main.e3ab0ec032a302c74f43.bundle.js"
+  },
+  {
+    "url": "vendors~main.e3ab0ec032a302c74f43.bundle.js.LICENSE.txt"
+  }
+]);
